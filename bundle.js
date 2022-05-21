03e2f0a8081282a0abd2981e7574d427c2440ce1
AbitofTetration <58894120+AbitofTetration@users.noreply.github.com>
2022-05-21 17:35:25 -0500

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -4723,7 +4723,8 @@
             id: "Replit-2TDM",
             type: "main",
             code: "replit-unknown-2",
-            at: "https://tekka-arras-placeholder.technokaguya.repl.co"
+            at: "https://tekka-arras-placeholder.technokaguya.repl.co",
+            prefer: !0
             // featured: 1, // For Featured Status.
           }
         ]
